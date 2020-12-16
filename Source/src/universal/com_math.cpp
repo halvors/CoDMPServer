3a9f0c56e2179dc4d6314299171063a7f4085ac1
KuteKetX <KuteKetBhop@gmail.com>
2020-12-16 12:00:23 -0700

    Rename param_2 to mat as it's a matrix type.

--- Source/src/universal/com_math.cpp
+++ Source/src/universal/com_math.cpp
@@ -1,10 +1,8 @@
-void __cdecl MatrixTransformVector44(float *vec, float *param_2, float *out) {
-    if (vec == out) {
-		return;
-    }
-	
-    *out = *vec * *param_2 + vec[1] * param_2[4] + vec[2] * param_2[8] + vec[3] * param_2[12];
-    out[1] = *vec * param_2[1] + vec[1] * param_2[5] + vec[2] * param_2[9] + vec[3] * param_2[13];
-    out[2] = *vec * param_2[2] + vec[1] * param_2[6] + vec[2] * param_2[10] + vec[3] * param_2[14];
-    out[3] = *vec * param_2[3] + vec[1] * param_2[7] + vec[2] * param_2[11] + vec[3] * param_2[15];
+void __cdecl MatrixTransformVector44(float *vec, float *mat, float *out) {
+	ASSERT(vec != out);
+
+    *out = *vec * *mat + vec[1] * mat[4] + vec[2] * mat[8] + vec[3] * mat[12];
+    out[1] = *vec * mat[1] + vec[1] * mat[5] + vec[2] * mat[9] + vec[3] * mat[13];
+    out[2] = *vec * mat[2] + vec[1] * mat[6] + vec[2] * mat[10] + vec[3] * mat[14];
+    out[3] = *vec * mat[3] + vec[1] * mat[7] + vec[2] * mat[11] + vec[3] * mat[15];
 }
